58f986e05512f9f7946ca462044c5f682c4152a8
Yusei Ito <me@yuseiito.com>
2024-02-18 12:04:57 +0000

    chore: Makefikeの共通部分をCommon.mkに掃き出し

---
+++ solo5-rs/examples/Common.mk
@@ -0,0 +1,46 @@
+ifndef LIBNAME
+	$(error Set LIBNAME before importing Common.mk)
+endif
+
+CC:= x86_64-solo5-none-static-cc
+LD:= x86_64-solo5-none-static-ld
+HVT:= solo5-hvt
+ELFTOOL := solo5-elftool
+LIBNAME := do_nothing
+KERNEL_PATH := kernel.hvt
+CARGO := cargo +nightly
+CARGO_FLAGS := -Zbuild-std --target x86_64-unknown-none
+LIB_KERNEL := target/x86_64-unknown-none/debug/lib$(LIBNAME).a
+
+kernel: manifest.o $(LIB_KERNEL) lib.o
+	$(LD) -z solo5-abi=hvt -o $(KERNEL_PATH) $(LIB_KERNEL) manifest.o ~/solo5/lib.o
+
+$(LIB_KERNEL): src/**.rs src/*.rs Cargo.toml Cargo.lock
+	$(CARGO) build $(CARGO_FLAGS) 
+
+manifest.c: manifest.json
+	$(ELFTOOL) gen-manifest manifest.json manifest.c
+
+manifest.o: manifest.c
+	$(CC) -z solo5-api=hvt -c -o manifest.o manifest.c
+
+.PHONY: lib.o
+lib.o:
+	$(CC) -c ~/solo5/bindings/lib.c
+
+.PHONY: build # yet another alias for 'make kernel'
+build:
+	$(MAKE) kernel
+
+.PHONY: run
+run: ${BLOCK}
+	$(HVT) --mem=1024 -- $(KERNEL_PATH)
+
+.PHONY: dev
+dev: kernel run
+
+.PHONY: clean
+clean:
+	cargo clean
+	- rm manifest.c manifest.o $(KERNEL_PATH) lib.o
+	
